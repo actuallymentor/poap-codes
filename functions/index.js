e34671d523485c9abe6eea2fb7f8cc5746419609
Mentor <mentor@palokaj.co>
2022-07-29 21:10:30 +0200

    💚 give the functions more power because patricio broke the thing with too ,many codes

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require("firebase-functions")
+const runtime = {
+	timeoutSeconds: 540,
+	memory: '1GB'
+}
 
 const { get_event_data_from_code, get_code_statuses } = require( './modules/codes' )
 exports.get_event_data_from_code = functions.https.onCall( get_event_data_from_code )
-exports.get_code_statuses = functions.https.onCall( get_code_statuses )
+exports.get_code_statuses = functions.runWith( runtime ).https.onCall( get_code_statuses )
